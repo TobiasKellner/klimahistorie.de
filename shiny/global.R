d112c44079732bb2aef66181b892ea645087e845
TobiasKellner <Tobias.Kellner@protonmail.com>
2025-02-20 19:10:54 +0000

    UPDATE: add database connection

--- shiny/global.R
+++ shiny/global.R
@@ -1,16 +1,32 @@
 # Packages ---------------------------------------------------------------
 
-library(bs4Dash)
 library(shiny)
+library(shinyjs)
+library(bs4Dash)
+library(htmltools)
 library(dplyr)
 library(lubridate)
 library(purrr)
 library(ggplot2)
 library(plotly)
-library(htmltools)
-library(shinyjs)
+library(leaflet)
+
+source("authentication/db_connection.R")
 
 
 # Database connection -----------------------------------------------------
 
+config <- config::get(file = "authentication/config.yml")
+
+con <- db_connection()
+
+onStop(function() {
+  dbDisconnect(con)
+  print("DB-connection closed")
+  })
+
+
+
+
+month_list <- c("Gesamtes Jahr","Januar","Februar","März","April","Mai","Juni","Juli","August","September","Oktober","November","Dezember")
 
