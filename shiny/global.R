9ee5161a4d19d3fb40a748098fd1e90a50c63ed9
TobiasKellner <Tobias.Kellner@protonmail.com>
2025-03-09 16:58:31 +0000

    UPDATE: add station list

--- shiny/global.R
+++ shiny/global.R
@@ -2,11 +2,13 @@
 
 library(shiny)
 library(shinyjs)
+library(shinycssloaders)
 library(bs4Dash)
 library(htmltools)
 library(dplyr)
 library(lubridate)
 library(purrr)
+library(stringr)
 library(ggplot2)
 library(plotly)
 library(leaflet)
@@ -27,6 +29,26 @@ onStop(function() {
 
 
 
+# create named month vector
+month_list <- setNames(
+  c(0, 1:12),
+  c(
+    "Gesamtes Jahr",
+    "Januar",
+    "Februar",
+    "März",
+    "April",
+    "Mai",
+    "Juni",
+    "Juli",
+    "August",
+    "September",
+    "Oktober",
+    "November",
+    "Dezember"
+  )
+)
 
-month_list <- c("Gesamtes Jahr","Januar","Februar","März","April","Mai","Juni","Juli","August","September","Oktober","November","Dezember")
-
+# query station list
+Stationsliste <- dbGetQuery(con, "SELECT DISTINCT \"Name_Station\" FROM \"Tagesdaten\"") %>%
+  pull(Name_Station)
